d785b9769dc82f3128f134ab2c7cf28133eda7da
marcosdevelopernet <marcosfc.developer@gmail.com>
2021-01-27 23:38:21 -0300

    arquivo alterados

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.send("<html><Body><h3>Portal de Noticias</h3></body></html>");
-})
+    res.render("home/index");
+});
+app.get('/formulario_inclusao_noticia', function(req, res){
+    res.render("admin/form_add_noticia");
+});
+app.get('/noticias', function(req, res){
+    res.render("noticias/noticias");
+});
 
 app.listen(3000, function(){
     console.log("Servidor do express rodando na porta 3000");
