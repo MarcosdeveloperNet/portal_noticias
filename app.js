709db749acbe43a3aca0aa2d892ed642fcb6543c
marcosdevelopernet <marcosfc.developer@gmail.com>
2021-01-29 00:00:26 -0300

    enviando os arquivos do dia 28/01

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-var express = require('express');
-var app = express();
-app.set('view engine', 'ejs');
+var app = require('./config/server');
 
-app.get('/', function(req, res){
-    res.render("home/index");
-});
-app.get('/formulario_inclusao_noticia', function(req, res){
-    res.render("admin/form_add_noticia");
-});
-app.get('/noticias', function(req, res){
-    res.render("noticias/noticias");
-});
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaHome = require('./app/routes/home')(app);
+var rotaFormulario = require('./app/routes/formulario')(app);
 
 app.listen(3000, function(){
-    console.log("Servidor do express rodando na porta 3000");
+    console.log("Servidor On");
 });
