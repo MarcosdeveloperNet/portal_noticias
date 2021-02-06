fecd3644e153455c70de900c1860f2f6dbd404e2
marcosdevelopernet <marcosfc.developer@gmail.com>
2021-02-06 11:50:13 -0300

    Refactore da conexão com o DB

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,13 +1,14 @@
-var dbConection = require('../../config/dbConection');
+//var dbConection = require('../../config/dbConection');
 
 module.exports = function(app){
 
-    var connection = dbConection();
-
     app.get('/noticias', function(req, res){
 
+        var connection = app.config.dbConection();
+        
         connection.query('select * from noticias', function(error, result){
             res.render("noticias/noticia", {noticias : result});
         });
     });
+
 };
--- config/dbConection.js
+++ config/dbConection.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
 
+var connMySQL = function(){
+    console.log('Conexão com o DB estabelecida.');
+
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'admin123@',
+        database: 'portal_noticias'
+    });
+};
+
 module.exports = function(){
-        return mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'admin123@',
-            database: 'portal_noticias'
-        });
-    };
+    console.log('O AutoLoad corregou a conexão do DB.');
+    return connMySQL;
+};
