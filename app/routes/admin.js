8e244e6d1469ee0c1cf4b580aca82b47dbbc6940
marcosdevelopernet <marcosfc.developer@gmail.com>
2021-02-11 00:47:44 -0300

    Alterações do dia 11/02 - Adição do express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,15 @@ module.exports = function(application){
         application.post('/noticia/salvar', function(req, res){
             var noticia = req.body;
 
+        //Validações do formulário
+        req.assert('titulo', 'O título da notícia é obrigatório.').notEmpty();
+        req.assert('resumo', 'O resumo da notícia é obrigatório.').notEmpty();
+        req.assert('resumo', 'O resumo deve ter entre 10 e 100 caracteres.').len(10, 100);
+        req.assert('autor', 'O autor da notícia é obrigatório.').notEmpty();
+        req.assert('data_noticia', 'A data da notícia é obrigatória.').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('noticia', 'A notícia é obrigatório.').notEmpty();
+
+
         var connection = application.config.dbConection();
         var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
